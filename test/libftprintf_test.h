f9d667b92a9406b84cdf171e04d39604b3009e0a
dnovak_home <dnovak@student.42.fr>
2024-06-05 13:13:35 +0200

    Start up

---
+++ test/libftprintf_test.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libftprintf_test.h                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dnovak <dnovak@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/24 16:44:22 by dnovak            #+#    #+#             */
+/*   Updated: 2024/06/05 12:14:58 by dnovak           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFTPRINTF_TEST_H
+# define LIBFTPRINTF_TEST_H
+# include "../libftprintf.h"
+# include <stdio.h> // printf, I/O streams, fileno, fflush
+# include <unistd.h> // close, open, dup
+# include <fcntl.h> // flags for open
+# include <stdlib.h> // system
+
+
+void	print_test_cases(void);
+int		test_stdout(char *s);
+
+#endif //LIBFTPRINTF_TEST_H
