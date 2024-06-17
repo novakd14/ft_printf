700a5accb05e22ad50b941a46ced6b12baee1a3d
dnovak_home <dnovak@student.42.fr>
2024-06-17 21:25:21 +0200

    Added unity testing, and rewrited function for flags. Currently working char, str and ptr. (better checkout functionality further)

---
+++ unity_test/libftprintf_test.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libftprintf_test.h                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dnovak <dnovak@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/24 16:44:22 by dnovak            #+#    #+#             */
+/*   Updated: 2024/06/09 14:35:59 by dnovak           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFTPRINTF_TEST_H
+# define LIBFTPRINTF_TEST_H
+# include "../libftprintf.h"
+# include "unity.h"
+# include <fcntl.h>  // flags for open
+# include <stdio.h>  // printf, fflush
+# include <unistd.h> // open, close, dup, dup2
+# define MAX_INT 2147483647
+# define MIN_INT -2147483648
+# define MAX_UINT 4294967295
+# define MIN_UINT 0
+
+#endif // LIBFTPRINTF_TEST_H
